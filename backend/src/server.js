1f35ce3210c21e9d8722440c9605fc74c4eaf156
Biniyam Agegnehu <biniyamagegnehu2@gmail.com>
2026-02-07 22:28:01 +0300

    write server.js code

---
+++ backend/src/server.js
@@ -0,0 +1,38 @@
+const app = require('./app');
+const connectDB = require('./config/database');
+
+const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
+
+// Start server
+const server = app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+// Handle uncaught exceptions
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+// Graceful shutdown
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated!');
+  });
+});
