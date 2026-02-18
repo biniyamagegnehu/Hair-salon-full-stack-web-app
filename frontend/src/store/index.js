1f272629d9c20354ba0bd9874a5e2ccb288adb09
Biniyam Agegnehu <biniyamagegnehu2@gmail.com>
2026-02-18 22:07:25 +0300

    update the store with payment

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,12 +3,14 @@ import authReducer from './slices/authSlice';
 import serviceReducer from './slices/serviceSlice';
 import appointmentReducer from './slices/appointmentSlice';
 import queueReducer from './slices/queueSlice';
+import paymentReducer from './slices/paymentSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     services: serviceReducer,
     appointments: appointmentReducer,
-    queue: queueReducer
+    queue: queueReducer,
+    payment: paymentReducer
   },
 });
