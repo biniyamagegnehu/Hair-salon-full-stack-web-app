622f563dfb9c322e904a9c7d63eb69587ec8cd85
Biniyam Agegnehu <biniyamagegnehu2@gmail.com>
2026-02-18 21:20:49 +0300

    update index.js with queue slice

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import serviceReducer from './slices/serviceSlice';
 import appointmentReducer from './slices/appointmentSlice';
+import queueReducer from './slices/queueSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     services: serviceReducer,
-    appointments: appointmentReducer
+    appointments: appointmentReducer,
+    queue: queueReducer
   },
 });
